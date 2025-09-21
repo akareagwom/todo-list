6fff4253a4c2f230fc17813d183ab722e92fd018
akareagwom <akareazikang20@gmail.com>
2025-09-21 14:49:02 +0100

    navbar done

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MdOutlineSearch, MdOutlineNotificationsNone,MdOutlineCalendarToday } from "react-icons/md";
 
 
 const Navbar = () => {
@@ -6,8 +7,14 @@ const Navbar = () => {
     <div className='bg-white shadow-xl text-[#1C1D22] text-[20px] font-700 font-bold  '>
       <div className="flex justify-between p-4">
         <h1>Welcome back, Vincent 👋</h1>
-        <div className="w-[269px] flex justify-between items-center">
-
+        <div className="w-[269px] flex justify-between text-2xl items-center">
+            <MdOutlineSearch/>
+            <MdOutlineNotificationsNone/>
+            <MdOutlineCalendarToday/>
+            <p className='text-[16px] font-600 font-semibold text-[#1C1D2280] '>19 May 2022</p>
+            <div className="rounded-[50%] p-2 bg-blue-200">
+                <p>AA</p>
+            </div>
         </div>
       </div>
     </div>
